4766379faaf556c4ca441d3d49917156928da1f5
thiagopnts <thiagopnts@gmail.com>
2014-08-21 11:37:30 -0300

    remove old hacks needed by older versions of rust

--- lib.rs
+++ lib.rs
@@ -1,13 +1,12 @@
 #![crate_name = "rustic"]
+#![crate_type = "lib"]
 #![allow(ctypes)]
 #![feature(globs)]
-#![feature(lang_items)]
-#![crate_type="lib"]
 #![no_std]
-
 extern crate core;
 
-#[allow(dead_code)]
+use platform::io;
+
 #[path = "arch/x86/"]
 mod platform {
   pub mod drivers;
@@ -15,11 +14,3 @@ mod platform {
 }
 
 pub mod kernel;
-
-#[lang = "begin_unwind"]
-extern fn begin_unwind(args: &core::fmt::Arguments, file: &str, line: uint) -> ! {
-  loop {}
-}
-
-#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
-#[lang = "eh_personality"] extern fn eh_personality() {}

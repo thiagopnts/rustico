620851cf6e545a1e27ea1455af944de77ea2d50f
thiagopnts <thiagopnts@gmail.com>
2014-08-21 11:38:10 -0300

    remove old hacks needed by older versions of rust

--- kernel/mod.rs
+++ kernel/mod.rs
@@ -1,11 +1,9 @@
 use platform::drivers::vga;
 use platform::io;
 
-#[start]
 #[no_mangle]
-pub unsafe fn main(_argc: int, _argv: *mut u8) -> int {
-  vga::clear_screen(vga::Black);
-  io::write("olarmundo");
+pub fn kmain() -> int {
+  io::write("olar");
   0
 }
 
